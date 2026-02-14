96a980c7adc480d089e6338c2564ea749d670c7c
cbf <malmax5@mail.ru>
2026-02-14 16:57:19 +0300

    feat(include): add application status, calculation context and server funcs TODO

---
+++ include/calculator.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <pvm_math_lib/calculator_math.hpp>
+
+enum class AppStatus
+{
+    SUCCESS = 0,
+    ERROR_PARSE = 1,
+    ERROR_VALIDATION = 2,
+    ERROR_MATH = 3,
+    ERROR_HELP = 4
+};
+
+struct CalcContext
+{
+    long long first;
+    long long second;
+    char operation;
+    long long result;
+    pvm_math_lib::MathStatus mathStatus;
+    AppStatus appStatus;
+};
+
+// TODO
+// void run();
+// void parse(CalcContext* ctx, int argc, char** argv);
+// void check(CalcContext* ctx,);
+// void calculate(CalcContext* ctx);
+// void print(CalcContext* ctx);
