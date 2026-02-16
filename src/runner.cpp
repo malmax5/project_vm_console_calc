ddcbe7c22bc0b530a90ff5edd564c2b02cd49434
cbf <malmax5@mail.ru>
2026-02-16 18:09:29 +0300

    implement calculating and run func

--- src/runner.cpp
+++ src/runner.cpp
@@ -5,10 +5,45 @@ extern char** g_argv;
 
 void run()
 {
+    CalcContext ctx = {0, 0, 0, 0, pvm_math_lib::MathStatus::OK, AppStatus::SUCCESS};
 
+    parse(&ctx, g_argc, g_argv);
+    if (ctx.appStatus == AppStatus::ERROR_HELP)
+    {
+        // TODO: implement print help func
+        return;
+    }
+
+    // TODO: implement checker func
+    if (ctx.appStatus == AppStatus::SUCCESS)
+    {
+        calculate(ctx);
+    }
 }
 
 void calculate(CalcContext* ctx);
 {
-
+    switch(ctx->operation)
+    {
+        case '+':
+            ctx->mathStatus = pvm_math_lib::add(ctx->first, ctx->second, ctx->result);
+            break;
+        case '-':
+            ctx->mathStatus = pvm_math_lib::sub(ctx->first, ctx->second, ctx->result);
+            break;
+        case '*':
+            ctx->mathStatus = pvm_math_lib::mul(ctx->first, ctx->second, ctx->result);
+            break;
+        case '/':
+            ctx->mathStatus = pvm_math_lib::div(ctx->first, ctx->second, ctx->result);
+            break;
+        case '^':
+            ctx->mathStatus = pvm_math_lib::pow(ctx->first, ctx->second, ctx->result);
+            break;
+        case '!':
+            ctx->mathStatus = pvm_math_lib::add(ctx->first, ctx->result);
+            break;
+        default:
+            ctx->appStatus = AppStatus::ERROR_VALIDATION;
+    }
 }
