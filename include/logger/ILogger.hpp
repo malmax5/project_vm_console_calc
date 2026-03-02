5cac05d62e86f40e9acfc5c2e9d20c50c79c12ed
cbf <malmax5@mail.ru>
2026-03-02 14:07:01 +0300

    feat(logger): implement a ILogger interface

--- include/logger/ILogger.hpp
+++ include/logger/ILogger.hpp
@@ -1,8 +1,19 @@
 #pragma once
 
+#include <string_view>
+
 namespace app_calculator::logger
 {
 
+class ILogger
+{
+public:
+    virtual ~ILogger() = default;
 
+    virtual void debug(std::string_view message) = 0;
+    virtual void info(std::string_view message) = 0;
+    virtual void warn(std::string_view message) = 0;
+    virtual void error(std::string_view message) = 0;
+};
 
 }
