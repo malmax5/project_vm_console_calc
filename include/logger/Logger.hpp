d8aeac13423b8b89beff2f3d09f8e9c0e65ca59b
cbf <malmax5@mail.ru>
2026-03-02 14:16:14 +0300

    feat(logger): implement a Logger class which inherits a ILogger interface

--- include/logger/Logger.hpp
+++ include/logger/Logger.hpp
@@ -1,10 +1,35 @@
 #pragma once
 
 #include "logger/ILogger.hpp"
+#include <memory>
 
 namespace app_calculator::logger
 {
 
+class Logger : public ILogger
+{
+public:
+    static Logger& instance();
+    
+    Logger(const Logger&) = delete;
+    Logger& operator=(const Logger&) = delete;
+    Logger(Logger&&) noexcept = delete;
+    Logger& operator=(Logger&&) noexcept = delete;
 
+    void debug(std::string_view message) override;
+    void info(std::string_view message) override;
+    void warn(std::string_view message) override;
+    void error(std::string_view message) override;
 
-}
+private:
+    Logger();
+    ~Logger() override;
+
+private:
+    struct Impl;
+
+private:
+    std::unique_ptr<Impl> pimpl;
+};
+
+} // namespace app_calculator::logger
