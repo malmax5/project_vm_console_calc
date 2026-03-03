b38759fab8b0c4487a721de5227ca3ab3d09089a
cbf <malmax5@mail.ru>
2026-03-03 15:49:11 +0300

    feat(parser): implement JsonParser class which inherits from interface

---
+++ include/parser/JsonParser.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "core/IParser.hpp"
+#include <memory>
+
+namespace app_calculator::pareser
+{
+
+class JsonParser : public core::IParser
+{
+public:
+    JsonParser();
+    ~JsonParser() override;
+
+    JsonParser(const JsonParser&) = delete;
+    JsonParser& operator=(JsonParser&) = delete;
+
+    models::CalculationTask parse(std::string_view input_data) override;
+
+private:
+    struct Impl;
+
+private:
+    std::unique_ptr<Impl> pimpl;
+};
+
+}
