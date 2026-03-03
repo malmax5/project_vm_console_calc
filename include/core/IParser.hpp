2dd69035b105bb7c548711db8f0032bd40d6fcaf
cbf <malmax5@mail.ru>
2026-03-03 15:44:26 +0300

    feat(parser): add an IPareser interface

---
+++ include/core/IParser.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "models/CalculationTask.hpp"
+#include <string_view>
+
+namespace app_calculator::core
+{
+
+class IParser
+{
+public:
+    virtual ~IParser() = default;
+
+    virtual models::CalculationTask parse(std::string_view inputData) = 0;
+};
+
+} // namespace app_calculator::core
