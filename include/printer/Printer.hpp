6b47b101ab1a7611d78ce93df504a19f00ee15df
cbf <malmax5@mail.ru>
2026-03-04 23:24:27 +0300

    feat(printer): create a Printer classs which inherits a IPrinter interface

---
+++ include/printer/Printer.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "core/IPrinter.hpp"
+#include "logger/ILogger.hpp"
+
+#include <memory>
+
+namespace app_calculator::printer
+{
+
+class Printer : public core::IPrinter
+{
+  public:
+    Printer();
+    ~Printer() override;
+
+    void print_result(long long result) const override;
+    void print_error(std::string_view message) const override;
+    void print_info(std::string_view message) const override;
+
+  private:
+    std::unique_ptr<logger::ILogger> _logger;
+};
+
+} // namespace app_calculator::printer
