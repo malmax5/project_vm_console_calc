e50e2c25720e3c132b7b2b7c4d434aa8626c71cd
cbf <malmax5@mail.ru>
2026-03-06 00:19:50 +0300

    refactor: apply Rule Of Five to IRunner

--- include/core/IRunner.hpp
+++ include/core/IRunner.hpp
@@ -8,7 +8,13 @@ namespace app_calculator::core
 class IRunner
 {
   public:
+    IRunner() = default;
     virtual ~IRunner() = default;
+
+    IRunner(const IRunner &) = delete;
+    IRunner &operator=(const IRunner &) = delete;
+    IRunner(IRunner &&) noexcept = delete;
+    IRunner &operator=(IRunner &&) noexcept = delete;
 
     virtual void run(std::string_view jsonInput) = 0;
 };
