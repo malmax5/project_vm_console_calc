8c59a034ec6b94b54787acc91d924dc977d3ec2a
cbf <malmax5@mail.ru>
2026-03-06 00:19:18 +0300

    refactor: apply Rule Of Five to IPrinter

--- include/core/IPrinter.hpp
+++ include/core/IPrinter.hpp
@@ -8,7 +8,13 @@ namespace app_calculator::core
 class IPrinter
 {
   public:
+    IPrinter() = default;
     virtual ~IPrinter() = default;
+
+    IPrinter(const IPrinter &) = delete;
+    IPrinter &operator=(const IPrinter &) = delete;
+    IPrinter(IPrinter &&) noexcept = delete;
+    IPrinter &operator=(IPrinter &&) noexcept = delete;
 
     virtual void print_result(long long result) const = 0;
     virtual void print_error(std::string_view message) const = 0;
