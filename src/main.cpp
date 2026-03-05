ce1e4ea5b8f9772fdb64bf99e91a321f0b7c73a5
cbf <malmax5@mail.ru>
2026-03-05 18:28:55 +0300

    refactor(main): replace procedural main with Runner orchestrator

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,32 @@
-#include "calculator.hpp"
+#include "runner/Runner.hpp"
+
+#include <iostream>
+#include <string>
 
 int main(int argc, char **argv)
 {
-    run(argc, argv);
+    using namespace app_calculator;
+    runner::Runner app;
+
+    if (argc > 1)
+    {
+        app.run(argv[1]);
+        return 0;
+    }
+
+    std::cout << "Enter JSON task: ";
+    std::string input;
+    std::string line;
+
+    while (std::getline(std::cin, line))
+    {
+        input += line;
+    }
+
+    if (!input.empty())
+    {
+        app.run(input);
+    }
 
     return 0;
 }
