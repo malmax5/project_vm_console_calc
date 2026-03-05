8110296b30edafff6e2bba7237c915c46c4247e2
cbf <malmax5@mail.ru>
2026-03-06 00:41:02 +0300

    refactor: apply Rule Of Five to ILogger

--- include/logger/ILogger.hpp
+++ include/logger/ILogger.hpp
@@ -8,7 +8,13 @@ namespace app_calculator::logger
 class ILogger
 {
   public:
+    ILogger() = default;
     virtual ~ILogger() = default;
+
+    ILogger(const ILogger &) = delete;
+    ILogger &operator=(const ILogger &) = delete;
+    ILogger(ILogger &&) noexcept = delete;
+    ILogger &operator=(ILogger &&) noexcept = delete;
 
     virtual void debug(std::string_view message) = 0;
     virtual void info(std::string_view message) = 0;
