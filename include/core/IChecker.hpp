6501b9c8fb7803ddcd0994de6a5a7dba2efbcbd5
cbf <malmax5@mail.ru>
2026-03-06 00:17:38 +0300

    refactor: apply Rule Of Five to IChecker

--- include/core/IChecker.hpp
+++ include/core/IChecker.hpp
@@ -8,7 +8,13 @@ namespace app_calculator::core
 class IChecker
 {
   public:
+    IChecker() = default;
     virtual ~IChecker() = default;
+
+    IChecker(const IChecker &) = delete;
+    IChecker &operator=(const IChecker &) = delete;
+    IChecker(IChecker &&) noexcept = delete;
+    IChecker &operator=(IChecker &&) noexcept = delete;
 
     virtual void check(const models::CalculationTask &task) = 0;
 };
