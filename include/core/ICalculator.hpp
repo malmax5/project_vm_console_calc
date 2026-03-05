d9c9b70949f8876fdd1d78b10822d9ee3227922e
cbf <malmax5@mail.ru>
2026-03-06 00:16:10 +0300

    refactor: apply Rule Of Five to ICalculator

--- include/core/ICalculator.hpp
+++ include/core/ICalculator.hpp
@@ -8,7 +8,13 @@ namespace app_calculator::core
 class ICalculator
 {
   public:
+    ICalculator() = default;
     virtual ~ICalculator() = default;
+
+    ICalculator(const ICalculator &) = delete;
+    ICalculator &operator=(const ICalculator &) = delete;
+    ICalculator(ICalculator &&) noexcept = delete;
+    ICalculator &operator=(ICalculator &&) noexcept = delete;
 
     virtual long long calculate(const models::CalculationTask &task) = 0;
 };
