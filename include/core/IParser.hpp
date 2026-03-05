a5b61aeda1a1646328d50a672c0f55f388421046
cbf <malmax5@mail.ru>
2026-03-06 00:18:40 +0300

    refactor: apply Rule Of Five to IParser

--- include/core/IParser.hpp
+++ include/core/IParser.hpp
@@ -9,7 +9,13 @@ namespace app_calculator::core
 class IParser
 {
   public:
+    IParser() = default;
     virtual ~IParser() = default;
+
+    IParser(const IParser &) = delete;
+    IParser &operator=(const IParser &) = delete;
+    IParser(IParser &&) noexcept = delete;
+    IParser &operator=(IParser &&) noexcept = delete;
 
     virtual models::CalculationTask parse(std::string_view inputData) = 0;
 };
