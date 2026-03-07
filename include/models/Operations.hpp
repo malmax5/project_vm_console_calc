0903f2e01e0249869cddf9f71cbcdb0230f6fe2e
cbf <malmax5@mail.ru>
2026-03-07 20:29:36 +0300

    feat(models): define global operationsInfo registry

--- include/models/Operations.hpp
+++ include/models/Operations.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "models/OperationInfo.hpp"
+
+#include <set>
 #include <string_view>
 
 namespace app_calculator::models::operations
@@ -11,5 +14,8 @@ inline constexpr std::string_view mul = "mul";
 inline constexpr std::string_view div = "div";
 inline constexpr std::string_view pow = "pow";
 inline constexpr std::string_view fact = "fact";
+
+inline const std::set<models::OperationInfo> operationsInfo = {{sum, 2}, {sub, 2}, {mul, 2},
+                                                               {div, 2}, {pow, 2}, {fact, 1}};
 
 } // namespace app_calculator::models::operations
