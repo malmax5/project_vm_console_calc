c2be65759d71099ec68273bbe2f957dfb5432eea
cbf <malmax5@mail.ru>
2026-03-21 00:12:58 +0300

    feat(db): introduce template DbRepository class to keep an pointer to database connectionb

---
+++ include/database/repositories/DbRepository.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "core/IDbConnection.hpp"
+#include <memory>
+
+namespace app_calculator::database
+{
+
+template <typename TResult> class DbRepository
+{
+  public:
+    explicit DbRepository(std::shared_ptr<core::IDbConnection<TResult>> dbConnection);
+    virtual ~DbRepository() = default;
+
+    DbRepository(const DbRepository &) = delete;
+    DbRepository &operator=(const DbRepository &) = delete;
+    DbRepository(DbRepository &&) noexcept = default;
+    DbRepository &operator=(DbRepository &&) noexcept = default;
+
+  protected:
+    auto &getDbConnection() const;
+
+  private:
+    std::shared_ptr<core::IDbConnection<TResult>> _dbConnection;
+};
+
+} // namespace app_calculator::database
