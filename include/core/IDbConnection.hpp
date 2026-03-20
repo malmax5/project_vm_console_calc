c7a2c7c1e8148e3994237e54c49621babde21738
cbf <malmax5@mail.ru>
2026-03-20 12:22:31 +0300

    feat(core): add rule of five into IDbConnection via clang-tidy

--- include/core/IDbConnection.hpp
+++ include/core/IDbConnection.hpp
@@ -8,7 +8,13 @@ namespace app_calculator::core
 template <typename TResult> class IDbConnection
 {
   public:
+    IDbConnection() = default;
     virtual ~IDbConnection() = default;
+    
+    IDbConnection(const IDbConnection &) = delete;
+    IDbConnection &operator=(const IDbConnection &) = delete;
+    IDbConnection(IDbConnection &&) noexcept = default;
+    IDbConnection &operator=(IDbConnection &&) noexcept = default;
 
     virtual TResult execute(const std::string &query) = 0;
     virtual bool isConnected() const = 0;
