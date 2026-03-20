a017037c544a0e21107cfa1fb917f261264a5632
cbf <malmax5@mail.ru>
2026-03-21 00:11:45 +0300

    feat(core): introduce templated IRepository interface

---
+++ include/core/IRepository.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <optional>
+#include <vector>
+
+namespace app_calculator::core
+{
+
+template <typename T> class IRepository
+{
+  public:
+    IRepository() = default;
+    virtual ~IRepository() = default;
+
+    IRepository(const IRepository &) = delete;
+    IRepository &operator=(const IRepository &) = delete;
+    IRepository(IRepository &&) noexcept = default;
+    IRepository &operator=(IRepository &&) noexcept = default;
+
+    virtual void add(const T &item) = 0;
+    virtual std::vector<T> getAll() const = 0;
+};
+
+} // namespace app_calculator::core
