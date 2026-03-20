2ecdf5279f77cddce7f7fe499217b3262ced27f2
cbf <malmax5@mail.ru>
2026-03-21 00:13:16 +0300

    feat(db): implement DbRepository calss

---
+++ src/database/repositories/DbRepository.cpp
@@ -0,0 +1,17 @@
+#include "database/repositories/DbRepository.hpp"
+
+namespace app_calculator::database
+{
+
+template <typename TResult> auto &DbRepository<TResult>::getDbConnection() const
+{
+    return *_dbConnection;
+}
+
+template <typename TResult>
+DbRepository<TResult>::DbRepository(std::shared_ptr<core::IDbConnection<TResult>> dbConnection)
+    : _dbConnection(std::move(dbConnection))
+{
+}
+
+} // namespace app_calculator::database
