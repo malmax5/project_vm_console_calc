54ebfaf213199dbb372382d3c9b7493817ba926e
cbf <malmax5@mail.ru>
2026-03-21 18:50:04 +0300

    fix(db): change the wrong variable name

--- src/database/repositories/HistoryRepository.cpp
+++ src/database/repositories/HistoryRepository.cpp
@@ -63,8 +63,8 @@ std::optional<int64_t> HistoryRepository::findResult(int64_t operandA,
     std::string query = fmt::format("SELECT result FROM calculation_history "
                                     "WHERE operand_a = {} AND operand_b = {} AND operation = {}"
                                     "AND status_id = {}",
-                                    "ORDER BY created_at DESC LIMIT 1;" operandA, velueB, operation,
-                                    static_cast<int>(models::OperationStatus::success));
+                                    "ORDER BY created_at DESC LIMIT 1;", operandA, valueB,
+                                    operation, static_cast<int>(models::OperationStatus::success));
 
     auto res = getDbConnection().execute(query);
 
