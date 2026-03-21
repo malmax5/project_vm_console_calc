1a004e016ddd8c8b1e430518fef8ea0553f85365
cbf <malmax5@mail.ru>
2026-03-21 16:30:44 +0300

    feat(operations): add commutative info to global variable: operationsInfo

--- include/models/Operations.hpp
+++ include/models/Operations.hpp
@@ -15,7 +15,12 @@ inline constexpr std::string_view div = "div";
 inline constexpr std::string_view pow = "pow";
 inline constexpr std::string_view fact = "fact";
 
-inline const std::set<models::OperationInfo> operationsInfo = {{sum, 2}, {sub, 2}, {mul, 2},
-                                                               {div, 2}, {pow, 2}, {fact, 1}};
+inline const std::set<models::OperationInfo> operationsInfo = {
+    {.name = sum, .expectedArgs = 2, .isCommutative = true},
+    {.name = sub, .expectedArgs = 2, .isCommutative = false},
+    {.name = mul, .expectedArgs = 2, .isCommutative = true},
+    {.name = div, .expectedArgs = 2, .isCommutative = false},
+    {.name = pow, .expectedArgs = 2, .isCommutative = false},
+    {.name = fact, .expectedArgs = 1, .isCommutative = false}};
 
 } // namespace app_calculator::models::operations
