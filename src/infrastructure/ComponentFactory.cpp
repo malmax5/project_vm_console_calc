1afd120d7d43f3bbbaceedcdfb72cc282ada4713
cbf <malmax5@mail.ru>
2026-03-23 01:03:10 +0300

    feat(inf): implement ComponentFactory::createProductionComponents func

---
+++ src/infrastructure/ComponentFactory.cpp
@@ -0,0 +1,39 @@
+#include "infrastructure/ComponentFactory.hpp"
+
+#include "calculator/CachedCalculator.hpp"
+#include "calculator/Calculator.hpp"
+#include "checker/Checker.hpp"
+#include "database/PostgresConnection.hpp"
+#include "database/repositories/CachedHistoryRepository.hpp"
+#include "database/repositories/HistoryRepository.hpp"
+#include "infrastructure/ConfigLoader.hpp"
+#include "models/DbConfig.hpp"
+#include "parser/JsonParser.hpp"
+#include "printer/Printer.hpp"
+
+namespace app_calculator::infrastructure
+{
+
+AppComponents ComponentFactory::createProductionComponents(const std::string &configPath)
+{
+    models::DbConfig config =
+        infrastructure::ConfigLoader::loadFromFile<models::DbConfig>(configPath);
+
+    auto dbConnection = std::make_shared<database::PostgresConnection>(config.connectionString());
+    auto historyRepository = std::make_unique<database::HistoryRepository>(dbConnection);
+    auto cachedRepository =
+        std::make_shared<database::CachedHistoryRepository>(std::move(historyRepository));
+
+    auto baseCalculator = std::make_unique<calculator::Calculator>();
+
+    AppComponents components;
+    components.parser = std::make_unique<parser::JsonParser>();
+    components.checker = std::make_unique<checker::Checker>();
+    components.printer = std::make_unique<printer::Printer>();
+    components.calculator =
+        std::make_unique<calculator::CachedCalculator>(std::move(baseCalculator), cachedRepository);
+
+    return components;
+}
+
+} // namespace app_calculator::infrastructure
