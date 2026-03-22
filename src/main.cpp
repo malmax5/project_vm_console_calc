4d10c8c0ea8ace587c1781221079ef22de40a26b
cbf <malmax5@mail.ru>
2026-03-23 01:05:02 +0300

    refactor(main): change an entry point from runner to CalculatorApp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
-#include "logger/Logger.hpp"
-#include "runner/Runner.hpp"
+#include "application/CalculatorApp.hpp"
 
 #include <exception>
 #include <iostream>
@@ -10,8 +9,7 @@ int main(int argc, char **argv)
 
     try
     {
-        runner::Runner app;
-        app.run(argc, argv);
+        CalculatorApp::run(argc, argv);
     }
     catch (const std::exception &e)
     {
