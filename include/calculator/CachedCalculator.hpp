19e954e1c7f116a4772d888c6496a4c95774f00c
cbf <malmax5@mail.ru>
2026-03-22 19:02:10 +0300

    feat(calculator): create proxy class to incapsulate logic of cache and calculator class

---
+++ include/calculator/CachedCalculator.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "core/ICalculator.hpp"
+#include "core/IRepository.hpp"
+#include "models/Calculation.hpp"
+#include "models/CalculationTask.hpp"
+
+#include <memory>
+
+namespace app_calculator::calculator
+{
+
+class CachedCalculator final : public core::ICalculator
+{
+  public:
+    CachedCalculator(std::unique_ptr<core::ICalculator> calculator,
+                     std::shared_ptr<core::IRepository<models::Calculation>> repository);
+    ~CachedCalculator() override;
+
+    CachedCalculator(const CachedCalculator &) = delete;
+    CachedCalculator &operator=(CachedCalculator &) = delete;
+    CachedCalculator(CachedCalculator &&) noexcept = default;
+    CachedCalculator &operator=(CachedCalculator &&) noexcept = default;
+
+    long long calculate(const models::CalculationTask &task) override;
+
+  private:
+    std::unique_ptr<core::ICalculator> _calculator;
+    std::shared_ptr<core::IRepository<models::Calculation>> _repository;
+};
+
+} // namespace app_calculator::calculator
