a8ec662066c1c81cc12b0a34befa7d6781989fb4
cbf <malmax5@mail.ru>
2026-03-22 22:25:49 +0300

    fix(models): add finding a members in JSON object 'database'

--- src/models/DbConfig.cpp
+++ src/models/DbConfig.cpp
@@ -16,11 +16,16 @@ void to_json(nlohmann::json &jsonData, const DbConfig &dbConfig)
 
 void from_json(const nlohmann::json &jsonData, DbConfig &dbConfig)
 {
-    jsonData.at("host").get_to(dbConfig.host);
-    jsonData.at("port").get_to(dbConfig.port);
-    jsonData.at("dbname").get_to(dbConfig.dbname);
-    jsonData.at("user").get_to(dbConfig.user);
-    jsonData.at("password").get_to(dbConfig.password);
+    if (jsonData.contains("database")) 
+    {
+        const auto& jsonDataAtDb = jsonData.at("database");
+        
+        jsonDataAtDb.at("host").get_to(dbConfig.host);
+        jsonDataAtDb.at("port").get_to(dbConfig.port);
+        jsonDataAtDb.at("dbname").get_to(dbConfig.dbname);
+        jsonDataAtDb.at("user").get_to(dbConfig.user);
+        jsonDataAtDb.at("password").get_to(dbConfig.password);
+    }
 }
 
 } // namespace app_calculator::models
