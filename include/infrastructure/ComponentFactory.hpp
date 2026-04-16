e0e017b9448fdcd1bd4c5e4ad48e01fb23310e95
cbf <malmax5@mail.ru>
2026-04-17 01:15:33 +0300

    refactor(inf): delete a runners creating via new entity of RunnerFabric

--- include/infrastructure/ComponentFactory.hpp
+++ include/infrastructure/ComponentFactory.hpp
@@ -1,32 +1,15 @@
 #pragma once
 
-#include "core/ICalculator.hpp"
-#include "core/IChecker.hpp"
-#include "core/IParser.hpp"
-#include "core/IPrinter.hpp"
-#include "core/IRunner.hpp"
+#include "infrastructure/AppContext.hpp"
 
 #include <memory>
 
 namespace app_calculator::infrastructure
 {
-
-struct AppComponents
-{
-  public:
-    std::unique_ptr<core::IParser> parser;
-    std::unique_ptr<core::IChecker> checker;
-    std::shared_ptr<core::IPrinter> printer;
-    std::unique_ptr<core::ICalculator> calculator;
-};
-
 class ComponentFactory
 {
   public:
-    static AppComponents createProductionComponents(const std::string &configPath);
-    static std::unique_ptr<core::IRunner> createConsoleRunner(const std::string &configPath,
-                                                              std::string &inputJson);
-    static std::unique_ptr<core::IRunner> createServerRunner(const std::string &configPath);
+    static AppContext createProductionContext(const std::string &configPath);
 };
 
 } // namespace app_calculator::infrastructure
--- src/infrastructure/ComponentFactory.cpp
+++ src/infrastructure/ComponentFactory.cpp
@@ -12,13 +12,13 @@
 #include "network/CalculatorServiceImpl.hpp"
 #include "parser/JsonParser.hpp"
 #include "printer/Printer.hpp"
-#include "runner/ConsoleRunner.hpp"
-#include "runner/ServerRunner.hpp"
+
+#include <grpcpp/grpcpp.h>
 
 namespace app_calculator::infrastructure
 {
 
-AppComponents ComponentFactory::createProductionComponents(const std::string &configPath)
+AppContext ComponentFactory::createProductionContext(const std::string &configPath)
 {
     models::DbConfig config =
         infrastructure::ConfigLoader::loadFromFile<models::DbConfig>(configPath);
@@ -28,37 +28,17 @@ AppComponents ComponentFactory::createProductionComponents(const std::string &co
 
     auto baseCalculator = std::make_unique<calculator::Calculator>();
 
-    AppComponents components;
-    components.parser = std::make_unique<parser::JsonParser>();
-    components.checker = std::make_unique<checker::Checker>();
-    components.printer = std::make_shared<printer::Printer>(logger::Logger::instance());
+    AppContext context;
+    context.parser = std::make_unique<parser::JsonParser>();
+    context.checker = std::make_unique<checker::Checker>();
+    context.printer = std::make_shared<printer::Printer>(logger::Logger::instance());
 
     auto cachedRepository = std::make_shared<database::CachedHistoryRepository>(
-        std::move(historyRepository), components.printer);
-    components.calculator =
+        std::move(historyRepository), context.printer);
+    context.calculator =
         std::make_unique<calculator::CachedCalculator>(std::move(baseCalculator), cachedRepository);
 
-    return components;
-}
-
-std::unique_ptr<core::IRunner> ComponentFactory::createConsoleRunner(const std::string &configPath,
-                                                                     std::string &inputJson)
-{
-    auto components = createProductionComponents(configPath);
-
-    return std::make_unique<runner::ConsoleRunner>(
-        std::move(components.parser), std::move(components.checker),
-        std::move(components.calculator), components.printer, inputJson);
-}
-
-std::unique_ptr<core::IRunner> ComponentFactory::createServerRunner(const std::string &configPath)
-{
-    AppComponents components = createProductionComponents(configPath);
-
-    auto service = std::make_shared<network::CalculatorServiceImpl>(
-        std::move(components.calculator), std::move(components.checker), components.printer);
-
-    return std::make_unique<runner::ServerRunner>(service);
+    return context;
 }
 
 } // namespace app_calculator::infrastructure
